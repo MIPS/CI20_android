e6a2c5fdf37d3319d06fee8deafd21c094f62afc
Dragan Cecavac <dragan.cecavac@imgtec.com>
2016-09-13 13:25:17 +0200

    Enable camera support
    pmem_camera is no longer needed in BOARD_KERNEL_CMDLINE since
    3.18 kernel uses ion instead of pmem.
    Kernel IPU driver only registeres /dev/ipu0 and there is no need
    for /dev/ipu1 anymore.
    Camera device is recognized as /dev/video0 instead of /dev/cim
    and provides V4L2 interface.
    
    Change-Id: I0df4867330653fee7ee252aea58f35f388c33ecf
    Signed-off-by: Dragan Cecavac <dragan.cecavac@imgtec.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ ARCH_MIPS_PAGE_SHIFT := 12
 TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x81F00000
-BOARD_KERNEL_CMDLINE := mem=256M@0x0 mem=752M@0x30000000 console=ttyS0,115200 ip=off rw rdinit=/init pmem_camera=16M@0x5f000000 androidboot.hardware=ci20
+BOARD_KERNEL_CMDLINE := mem=256M@0x0 mem=752M@0x30000000 console=ttyS0,115200 ip=off rw rdinit=/init androidboot.hardware=ci20
 ifneq ($(WITH_EXT4),true)
 BOARD_KERNEL_CMDLINE += ubi.mtd=1
 endif
