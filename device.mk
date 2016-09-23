4bf7243437f08161a4919c1d5d545602497b185a
Lazar Trsic <Lazar.Trsic@imgtec.com>
2016-09-23 17:49:45 +0200

    Enable camera module for CI20
    Grant cameraserver needed sepolicy rules.
    
    Change-Id: Ie5585e56da37a2cc123e1ea35d34cd3b69a74157

--- device.mk
+++ device.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES +=     \
 
 PRODUCT_PACKAGES +=      \
     hwcomposer.xb4780    \
+    camera.xb4780        \
     audio.primary.xb4780 \
     audio_policy.xb4780  \
     lights.xb4780        \
