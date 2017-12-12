9717d259cf5b08a6b979ecff850328bd3be492bb
Dragan Cecavac <dragan.cecavac@mips.com>
2017-12-12 15:44:51 +0100

    Introduce loadable dtb support
    dtb/src/ci20-sdcard.dts and dtb/src/ci20-nand.dts empty files
    have been added as a foundation for sdcard/nand specific device
    tree support. These files will be combined with kernel device tree
    specification when device tree blobs are created.
    
    sdcard scripts have been updated to burn dtb and arrange U-Boot to
    load dtb to 0x76000000 address.
    
    build-dtb.sh script has been introduced. It creates dtb/ci20-sdcard.dtb and
    dtb/ci20-nand.dtb prebuilt dtbs based on content of dtb/src directory and
    files included from kernel sources specified by --kernel_dir= option.
    
    Change-Id: I0cd24986d370781c9b02460af02a55b52f302aa1
    Signed-off-by: Dragan Cecavac <dragan.cecavac@mips.com>

---
+++ dtb/build-dtb.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 Mips Tech LLC
+# Author: Dragan Cecavac <dragan.cecavac@mips.com>
+#
+# Usage:
+#	./build-dtb.sh --kernel_dir=<kernel-src-dir-path>
+#
+
+set -e
+tmpDir=`mktemp -d`
+
+cleanup()
+{
+	rm -rf ${tmpDir}
+	trap - EXIT INT TERM
+}
+trap cleanup EXIT INT TERM
+
+die()
+{
+	echo "$@" >&2
+	exit 1
+}
+
+for OPT; do
+	OPTARG=$(expr "x$OPT" : "x[^=]*=\\(.*\\)" || true)
+	case $OPT in
+		--kernel_dir=*)
+			KERNEL_DIR=$OPTARG
+			;;
+		--help|-?)
+			echo "Usage : build-dtb.sh --kernel_dir=<kernel-src-dir-path>"
+			exit 0
+			;;
+		*)
+			echo "Unrecognized option ${OPT}"
+			echo "Type --help or -? for usage information."
+			die
+			;;
+	esac
+done
+
+[ ! -z "${KERNEL_DIR}" ] || die "Please specify kernel source location using option --kernel_dir="
+[ ! -d "${KERNEL_DIR}" ] && die "Specified kernel source directory does not exist"
+
+# Check for cpp
+[ -z `which cpp 2>&1` ] && die "No 'cpp' present"
+
+# Check for dtc
+[ -z `which dtc 2>&1` ] && die "No 'dtc' present"
+
+# initialize dtsPrebuiltDir with the script's location
+pushd `dirname $0` &> /dev/null
+dtsPrebuiltDir=`pwd`
+popd &> /dev/null
+
+cp $KERNEL_DIR/arch/mips/boot/dts/jz4780.dtsi $tmpDir/
+
+dts_id=("sdcard" "nand")
+
+for i in "${dts_id[@]}"
+do
+	ID=ci20-$i
+	SRC=$tmpDir/$ID.dts
+	TMP=$tmpDir/$ID.tmp.dts
+	DST=$dtsPrebuiltDir/$ID.dtb
+
+	# Append kernel device tree source with device tree specified in Android device project
+	cat $KERNEL_DIR/arch/mips/boot/dts/ci20.dts $dtsPrebuiltDir/src/$ID.dts > $SRC
+
+	cpp -nostdinc -I $KERNEL_DIR/include -undef -x assembler-with-cpp $SRC > $TMP
+	dtc -O dtb -b 0 -o $DST $TMP
+done
